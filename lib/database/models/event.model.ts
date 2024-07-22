9324e804e9d535166ed0ad79f983780e874a271a
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-07-22 16:09:29 +0530

    event creation UI + connection to backend to create events in the database

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -14,7 +14,7 @@ export interface IEvent extends Document {
     url?: string;
     category: { _id: string, name: string };
     organizer: { _id: string, firstName: string, lastName: string };    
-}2
+}
 
 const EventSchema = new Schema({
     title: { type: String, required: true },
