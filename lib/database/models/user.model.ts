aff3e4ac599a4a3dadb4edd062d7d268045ae84b
Abhay Sahoo <abhayshaw19999@gmail.com>
2024-07-27 19:41:26 +0530

    bug fix - allowing user creation with username, first_name or last_name coming up as null

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -3,10 +3,10 @@ import { model, models, Schema } from "mongoose";
 const UserSchema = new Schema({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    username: { type: String, unique: true },
-    firstName: { type: String},
-    lastName: { type: String},
-    photo: { type: String},
+    username: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    photo: { type: String, required: true },
 })
 
 const User = models.User || model('User', UserSchema);
